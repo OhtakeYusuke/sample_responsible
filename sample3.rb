1118773e0e4708a9ca7188bd72deeec05101f2c1
OhtakeYusuke <o.yusuke0501@gmail.com>
2021-01-03 20:00:24 +0900

    企画下書き練習

--- sample3.rb
+++ sample3.rb
@@ -1,46 +1,12 @@
-class User
-  attr_reader :name, :age
-
-  def initialize(name:, age:)
-    @name = name
-    @age = age
-  end
+puts "6個のりんごを山分けします。山分けする人数を入力してください。"
+num = gets.to_i
+begin
+if 6 % num != 0
+raise RuntimeError, "山分けできません"
 end
-
-class Zoo
-  def  initialize(name:, entry_fee:)
-    # nameが今回の課題必要なさそうだったので、今回はコメントアウト
-    # @name = name
-    @entry_fee = entry_fee
-  end
-
-  def info_entry_fee(user)
-    puts "#{user.name}さんの入場料金は#{entry_fee_judge(user)}円です。"
-  end
-
-  def entry_fee_judge(user)
-    if user.age <= 7 && user.age >= 0
-      @entry_fee[:infant]
-    elsif user.age <= 18
-      @entry_fee[:children]
-    elsif user.age <= 65
-      @entry_fee[:adult]
-    else
-      @entry_fee[:senior]
-    end
-  end
-
-end
-
-zoo = Zoo.new(name: "旭山動物園", entry_fee: { infant: 0, children: 400, adult: 800, senior: 500 })
-
-users = [
-  User.new(name: "たま", age: 3),
-  User.new(name: "ゆたぼん", age: 10),
-  User.new(name: "あじー", age: 32),
-  User.new(name: "ぎん", age: 108)
-]
-
-users.each do |user|
-  zoo.info_entry_fee(user)
+yourNum = 6 / num
+puts "山分けすると1人#{6/num}個です"
+rescue => e
+puts "1人以上の人数を数値で入力してください"
+puts "例外は #{e} です"
 end
